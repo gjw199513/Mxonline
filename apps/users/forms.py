c3e346ea79b7795ba390c29f5bfe75a6c01442d8
gjw199513 <gjw605134015>
2017-02-08 22:49:19 +0800

    登录注册找回密码form

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -6,25 +6,33 @@ from django import forms
 from captcha.fields import CaptchaField
 from .models import UserProfile
 
+
+# 登录form
 class LoginForm(forms.Form):
+    # required=True该字段为必填字段
     username = forms.CharField(required=True)
     password = forms.CharField(required=True, min_length=5)
 
 
+# 注册form
 class RegisterForm(forms.Form):
     email = forms.EmailField(required=True)
     password = forms.CharField(required=True, min_length=5)
+    # 验证码使用
     captcha = CaptchaField(error_messages={"invalid": u"验证码错误"})
 
 
+# 忘记密码form
 class ForgetForm(forms.Form):
     email = forms.EmailField(required=True)
     captcha = CaptchaField(error_messages={"invalid": u"验证码错误"})
 
 
+# 密码修改form
 class ModifyPwdForm(forms.Form):
     password1 = forms.CharField(required=True, min_length=5)
     password2 = forms.CharField(required=True, min_length=5)
+
 
 class UploadImageForm(forms.ModelForm):
     class Meta:
