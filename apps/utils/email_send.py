beecbfbc68de05fc0a1a64b315d98b23c10a370b
gjw199513 <gjw605134015>
2017-02-08 22:48:23 +0800

    发送邮件注册及忘记密码

--- apps/utils/email_send.py
+++ apps/utils/email_send.py
@@ -8,22 +8,26 @@ from users.models import EmailVerifyRecord
 from Mxonline.settings import  EMAIL_FROM
 
 
+# 生成随机字符串函数
 def random_str(randomlength=8):
     str = ''
     chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789'
     length = len(chars) - 1
     random = Random()
     for i in range(randomlength):
-        str+=chars[random.randint(0,length)]
+        str += chars[random.randint(0,length)]
     return str
 
 
+# 发送邮箱验证码
 def send_register_email(email, send_type="register"):
+    # 创建邮箱验证码实例
     email_record = EmailVerifyRecord()
     if send_type == "update_email":
         code = random_str(4)
     else:
         code = random_str(16)
+    # 将数据验证码、邮箱保存、验证类型保存
     email_record.code = code
     email_record.email = email
     email_record.send_type = send_type
@@ -33,6 +37,7 @@ def send_register_email(email, send_type="register"):
     email_body = ""
 
     if send_type == 'register':
+        # 邮件发送标题和内容
         email_title = '慕学在线网注册激活链接'
         email_body = "请点击下面的链接激活你的账号：http://127.0.0.1:8000/active/{0}".format(code)
 
@@ -41,7 +46,7 @@ def send_register_email(email, send_type="register"):
             pass
 
     elif send_type == 'forget':
-        email_title = '慕学在线网注册码重置链接'
+        email_title = '慕学在线网密码重置链接'
         email_body = "请点击下面的链接重置你的密码：http://127.0.0.1:8000/reset/{0}".format(code)
 
         send_status = send_mail(email_title, email_body, EMAIL_FROM, [email])
