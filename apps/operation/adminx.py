2962e5c79ad2225641471deb57552e015353c6c5
gjw199513 <gjw605134015>
2017-02-08 11:27:33 +0800

    operation app的model注册

--- apps/operation/adminx.py
+++ apps/operation/adminx.py
@@ -6,30 +6,35 @@ import xadmin
 from .models import UserAsk, UserCourse, UserMessage, CourseComments, UserFavorite
 
 
+# 用户咨询注册
 class UserAskAdmin(object):
     list_display = ['name', 'mobile', 'course_name', 'add_time']
     search_fields = ['name', 'mobile', 'course_name']
     list_filter = ['name', 'mobile', 'course_name', 'add_time']
 
 
+# 用户课程注册
 class UserCourseAdmin(object):
     list_display = ['user', 'course', 'add_time']
     search_fields = ['user', 'course']
     list_filter = ['user', 'course', 'add_time']
 
 
+# 用户信息注册
 class UserMessageAdmin(object):
     list_display = ['user', 'message', 'has_read','add_time']
     search_fields = ['user', 'message', 'has_read']
     list_filter = ['user', 'message', 'has_read','add_time']
 
 
+# 课程评论注册
 class CourseCommentsAdmin(object):
     list_display = ['user', 'course', 'comments', 'add_time']
     search_fields = ['user', 'course', 'comments']
     list_filter = ['user', 'course', 'comments', 'add_time']
 
 
+# 用户收藏注册
 class UserFavoriteAdmin(object):
     list_display = ['user', 'fav_id', 'fav_type', 'add_time']
     search_fields = ['user', 'fav_id', 'fav_type']
