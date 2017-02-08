50731ec66a285b02e68f748b5790816e7f6493ec
gjw199513 <gjw605134015>
2017-02-08 11:15:40 +0800

    organization app的model注册

--- apps/organization/adminx.py
+++ apps/organization/adminx.py
@@ -7,12 +7,14 @@ import xadmin
 from .models import CityDict, CourseOrg, Teacher
 
 
+# 城市注册
 class CityDictAdmin(object):
     list_display = ['name', 'desc', 'add_time']
     search_fields = ['name', 'desc']
     list_filter = ['name', 'desc', 'add_time']
 
 
+# 课程机构注册
 class CourseOrgAdmin(object):
     list_display = ['name', 'desc', 'click_nums', 'fav_nums', 'address', 'city', 'add_time']
     search_fields = ['name', 'desc', 'click_nums', 'fav_nums', 'address', 'city']
@@ -20,10 +22,11 @@ class CourseOrgAdmin(object):
     relfield_style = "fk-ajax"
 
 
+# 教师注册
 class TeacherAdmin(object):
-    list_display = ['org', 'name', 'work_years', 'work_company', 'work_position', 'points', 'click_nums', 'fav_nums','add_time']
+    list_display = ['org', 'name', 'work_years', 'work_company', 'work_position', 'points', 'click_nums', 'fav_nums', 'add_time']
     search_fields = ['org', 'name', 'work_years', 'work_company', 'work_position', 'points', 'click_nums', 'fav_nums']
-    list_filter = ['org', 'name', 'work_years', 'work_company', 'work_position', 'points', 'click_nums', 'fav_nums','add_time']
+    list_filter = ['org', 'name', 'work_years', 'work_company', 'work_position', 'points', 'click_nums', 'fav_nums', 'add_time']
 
 
 xadmin.site.register(CityDict, CityDictAdmin)
