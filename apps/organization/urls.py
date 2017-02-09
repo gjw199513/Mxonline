4d7b722a335b8c2f2010d4f38b04a655f656fb96
gjw199513 <gjw605134015>
2017-02-09 23:19:01 +0800

    课程机构URL配置

--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -9,13 +9,18 @@ from .views import TeacherListView, TeacherDetailView
 urlpatterns = [
     # 课程机构列表页
     url(r'^list/$', OrgView.as_view(), name='org_list'),
+    # 我要学习咨询
     url(r'add_ask/$', AddUserAskView.as_view(), name='add_ask'),
+    # 机构首页
     url(r'home/(?P<org_id>\d+)/$', OrgHomeView.as_view(), name='org_home'),
+    # 机构课程列表页
     url(r'course/(?P<org_id>\d+)/$', OrgCourseView.as_view(), name='org_course'),
+    # 机构介绍列表页
     url(r'desc/(?P<org_id>\d+)/$', OrgDescView.as_view(), name='org_desc'),
+    # 机构讲师
     url(r'org_teacher/(?P<org_id>\d+)/$', OrgTeacherView.as_view(), name='org_teacher'),
 
-    #机构收藏
+    # 机构收藏
     url(r'add_fav/$', AddFavView.as_view(), name='add_fav'),
 
     # 教师收藏
