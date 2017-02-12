9954af38ab46f67d8c647b5a8c34a7177dbc2e54
gjw199513@163.com <gjw605134015>
2017-02-12 08:49:54 +0800

    课程url配置

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -9,19 +9,19 @@ urlpatterns = [
     # 课程列表页
     url(r'^list/$', CourseListView.as_view(), name='course_list'),
 
-    #课程详情页
+    # 课程详情页
     url(r'^detail/(?P<course_id>\d+)/$', CourseDetailView.as_view(), name='course_detail'),
 
-    #课程信息
+    # 课程章节信息
     url(r'^info/(?P<course_id>\d+)/$', CourseInfoView.as_view(), name='course_info'),
 
-    #课程评论
+    # 课程评论
     url(r'^comments/(?P<course_id>\d+)/$', CommentsView.as_view(), name='course_comments'),
 
-    #添加课程评论
+    # 添加课程评论
     url(r'^add_comment/$', AddComentsView.as_view(), name='add_comment'),
 
-
+    # 视频播放页面
     url(r'^video/(?P<video_id>\d+)/$', VideoPlayView.as_view(), name='video_play'),
 
 ]
